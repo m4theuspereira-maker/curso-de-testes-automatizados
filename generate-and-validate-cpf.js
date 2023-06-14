768f8bfcf898c1dbe9b54d37bb9374a44715f4e7
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-06-14 11:23:46 -0300

    first commit

---
+++ generate-and-validate-cpf.js
@@ -0,0 +1,73 @@
+let create_array = (total, numero) =>
+  Array.from(Array(total), () => number_random(numero));
+let number_random = (number) => Math.round(Math.random() * number);
+let mod = (dividendo, divisor) =>
+  Math.round(dividendo - Math.floor(dividendo / divisor) * divisor);
+
+function cpf(mask) {
+  let total_array = 9;
+  let n = 9;
+  let [n1, n2, n3, n4, n5, n6, n7, n8, n9] = create_array(total_array, n);
+
+  let d1 =
+    n9 * 2 +
+    n8 * 3 +
+    n7 * 4 +
+    n6 * 5 +
+    n5 * 6 +
+    n4 * 7 +
+    n3 * 8 +
+    n2 * 9 +
+    n1 * 10;
+  d1 = 11 - mod(d1, 11);
+  if (d1 >= 10) d1 = 0;
+
+  let d2 =
+    d1 * 2 +
+    n9 * 3 +
+    n8 * 4 +
+    n7 * 5 +
+    n6 * 6 +
+    n5 * 7 +
+    n4 * 8 +
+    n3 * 9 +
+    n2 * 10 +
+    n1 * 11;
+  d2 = 11 - mod(d2, 11);
+  if (d2 >= 10) d2 = 0;
+
+  if (mask) return `${n1}${n2}${n3}.${n4}${n5}${n6}.${n7}${n8}${n9}-${d1}${d2}`;
+  else return `${n1}${n2}${n3}${n4}${n5}${n6}${n7}${n8}${n9}${d1}${d2}`;
+}
+
+function validateCpf(cpf) {
+  cpf = cpf.replace(/[^\d]+/g, ""); // Remove caracteres não numéricos
+
+  if (cpf.length !== 11 || /^(.)\1+$/.test(cpf)) {
+    return false; // CPF deve ter 11 dígitos e não pode ser composto por dígitos repetidos
+  }
+
+  // Validação do primeiro dígito verificador
+  let soma = 0;
+  for (let i = 0; i < 9; i++) {
+    soma += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let resto = (soma * 10) % 11;
+  if (resto === 10 || resto === parseInt(cpf.charAt(9))) {
+    // Validação do primeiro dígito verificador passou, continue com o segundo
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+      soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === parseInt(cpf.charAt(10))) {
+      return true; // CPF válido
+    }
+  }
+
+  return false; // CPF inválido
+}
+
+const cpfGenerated = cpf(true);
+console.log(cpfGenerated);
+console.log(validateCpf(cpfGenerated));
