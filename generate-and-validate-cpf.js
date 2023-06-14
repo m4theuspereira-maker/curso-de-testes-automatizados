617561d592b63d0f221c77e6a7aa72a4b2cd9bdc
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-06-14 11:24:42 -0300

    chore: remove useless comments

--- generate-and-validate-cpf.js
+++ generate-and-validate-cpf.js
@@ -41,31 +41,29 @@ function cpf(mask) {
 }
 
 function validateCpf(cpf) {
-  cpf = cpf.replace(/[^\d]+/g, ""); // Remove caracteres não numéricos
+  cpf = cpf.replace(/[^\d]+/g, "");
 
   if (cpf.length !== 11 || /^(.)\1+$/.test(cpf)) {
-    return false; // CPF deve ter 11 dígitos e não pode ser composto por dígitos repetidos
+    return false;
   }
 
-  // Validação do primeiro dígito verificador
   let soma = 0;
   for (let i = 0; i < 9; i++) {
     soma += parseInt(cpf.charAt(i)) * (10 - i);
   }
   let resto = (soma * 10) % 11;
   if (resto === 10 || resto === parseInt(cpf.charAt(9))) {
-    // Validação do primeiro dígito verificador passou, continue com o segundo
     soma = 0;
     for (let i = 0; i < 10; i++) {
       soma += parseInt(cpf.charAt(i)) * (11 - i);
     }
     resto = (soma * 10) % 11;
     if (resto === 10 || resto === parseInt(cpf.charAt(10))) {
-      return true; // CPF válido
+      return true;
     }
   }
 
-  return false; // CPF inválido
+  return false;
 }
 
 const cpfGenerated = cpf(true);
